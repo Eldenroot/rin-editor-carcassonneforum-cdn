25af84920b2717c94d5621c63bc771f93ca9f01c
Eldenroot <Eldenroot@gmail.com>
2021-06-16 20:57:15 +0200

    Add files (customized)

---
+++ inc/languages/czech/rineditor.lang.php
@@ -0,0 +1,5 @@
+<?php
+$l['rineditor_restore'] = 'Obnovit';
+$l['rineditor_uploading'] = ' obrázky se nahrávají na server...';
+$l['rineditor_fail'] = 'Nepodařilo se nahrát obrázky:';
+?>
---
+++ jscripts/rin/editor/config.js
@@ -0,0 +1,64 @@
+/** Upraveno pro 1.1.8 - pridana podpora pro: tabulky, podbarveni textu, vlozeni obrazku do editoru pomoci klavesove zkratky Ctrl+V
+ * @license Copyright (c) 2003-2020, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here.
+	// For complete reference see:
+	// http://docs.ckeditor.com/#!/api/CKEDITOR.config
+
+	if ((CKEDITOR.env.mobile || CKEDITOR.env.iOS) && parseInt(rinmobsms)) {
+		rinstartupmode = 'source';
+	}
+
+	// The toolbar groups arrangement, optimized for a single toolbar row.
+	config.toolbarGroups = [
+		{ name: 'basicstyles' },
+		{ name: 'align' },
+		{ name: 'styles' },
+		{ name: 'colors', groups: [ 'colors', 'cleanup' ] },
+		{ name: 'insert' },
+		{ name: 'links' },
+		{ name: 'list' },
+		{ name: 'blocks', groups: [ 'blocks', 'clipboard' ] },
+		{ name: 'extra', groups: [ 'extra', 'extradesc' ] },
+		{ name: 'undo' },
+		{ name: 'document',	groups: [ 'tools', 'mode' ] }
+	];
+
+	config.extraPlugins = 'filetools,imgur_drag,notificationaggregator,uploadimage,uploadwidget,table';	
+	config.language = rinlanguage;
+	config.removePlugins = rinautosave;
+
+	// The default plugins included in the basic setup define some buttons that
+	// are not needed in a basic editor. They are removed here.
+	config.removeButtons = 'Cut,Copy,Paste,Anchor,indent,'+rinrmvbut+'';
+
+	// Dialog windows are also simplified.
+	config.removeDialogTabs = 'link:advanced';
+
+	config.contentsCss = content_url;
+	config.title = false;
+	config.image_prefillDimensions = parseInt(rin_img_resize) ? true : false;
+	config.height = rinheight;
+	config.fontSize_sizes = 'xx-small;x-small;small;medium;large;x-large;xx-large';
+	config.smiley_images = dropdownsmiliesurl.concat(dropdownsmiliesurlmore);
+	config.smiley_descriptions = dropdownsmiliesname.concat(dropdownsmiliesnamemore);
+	config.smiley_name = dropdownsmiliesdes.concat(dropdownsmiliesdesmore);
+	config.smiley_path = smileydirectory;
+	config.smiley_sc = rinsmileysc;
+	config.startupMode = rinstartupmode;
+	config.imgurClientId = rinimgur;
+	config.disableNativeSpellChecker = false;
+	config.skin = rinskin;
+	config.image_previewText = ' ';
+	config.background_color_support = true;
+	config.base64_unofficial_support = true;
+	config.imageUploadUrl = 'https://api.imgur.com/3/image';
+
+	config.autosave = {
+		saveDetectionSelectors : 'input[name*="submit"],input[name*="savedraft"],input[id*="quick_reply_submit"],input[name*="previewpost"]',
+		messageType : rinautosavemsg
+	};
+};
